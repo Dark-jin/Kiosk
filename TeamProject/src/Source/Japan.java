ab560e068bea4bc066da4206ddd29a88aaf5a09c
MoonSangWon <45040095+MoonSangWon@users.noreply.github.com>
2022-01-16 16:28:16 +0900

    수정 3차

--- TeamProject/src/Source/Japan.java
+++ TeamProject/src/Source/Japan.java
@@ -3,6 +3,13 @@ import java.awt.*;
 import javax.swing.*;
 public class Japan extends JFrame{	
 	public Japan() {
+		setTitle("¤·¤·");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
+		
+		
+		setSize(1500,1000);
+		setVisible(true);
+		setLocationRelativeTo(null);
 	}
 }
