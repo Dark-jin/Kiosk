9e475e5b99dddfef7e77f78b7a9b84ff22cd16a8
chch4lee1@naver.com <chch4lee1@naver.com>
2022-01-16 16:17:15 +0900

    한식 일식 수정

--- TeamProject/src/Source/Scar.java
+++ TeamProject/src/Source/Scar.java
@@ -7,10 +7,12 @@ public class Scar extends JFrame{
 	private Image img = icon.getImage();
 	private Image changeImg =img.getScaledInstance(100,100,Image.SCALE_SMOOTH);
 	private ImageIcon change = new ImageIcon(changeImg);
-	private JButton btn[] =new JButton[3];
-	private String name[] = {"양식","중식","일식"};
-	private JLabel label[] = new JLabel[3];
+	private JButton btn[] =new JButton[4];
+	private String name[] = {"양식","중식","일식","한식"};
+	private JLabel label[] = new JLabel[4];
 	public Scar() {
+		setTitle("키오스크");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setLayout(null);
 		for(int i=0;i<name.length;i++) {
 			btn[i] = new JButton();
@@ -23,30 +25,29 @@ public class Scar extends JFrame{
 			label[i].setFont(new Font("배달의민족 한나", 1, 20));
 			add(btn[i]);add(label[i]);
 			btn[i].addActionListener(new ActionListener() {
-				@Override
-				public void actionPerformed(ActionEvent e) {
-					JButton button =(JButton)e.getSource();
-					int num = getButtonIndex(button);
-					if(num==0) {new Yang();}
-					else if(num==1) {new China();}
-					else {new Japan();}
-					setVisible(false);
-				}
-			});
-		}
-		setTitle("키오스크");
-		setSize(1500,1000);
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setVisible(true);
-		setLocationRelativeTo(null);
+	            @Override
+	            public void actionPerformed(ActionEvent e) {
+	               JButton button =(JButton)e.getSource();
+	               int num = getButtonIndex(button);
+	               if(num==0) {new Yang();}
+	               else if(num==1) {new China();}
+	               else if(num==2) {new Japan();}
+	               else {new Korea();}
+	               setVisible(false);
+	            }
+	         });
+	      }
+	      setSize(1500,1000);
+	      setVisible(true);
+	      setLocationRelativeTo(null);
+	   }
+	   public int getButtonIndex(JButton button){
+	       int num = 0;
+	       for(int i =0; i<4; i++){
+	          if(btn[i] == button){
+	             num = i;
+	          }
+	       }
+	       return num;
+	    }
 	}
-	public int getButtonIndex(JButton button){
-       int num = 0;
-       for(int i =0; i<3; i++){
-          if(btn[i] == button){
-             num = i;
-          }
-       }
-       return num;
-    }
-}
