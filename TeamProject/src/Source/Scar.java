ba872e49880aed85ae7a1adb8437a790120a7b49
MoonSangWon <45040095+MoonSangWon@users.noreply.github.com>
2022-02-12 14:29:57 +0900

    02/12 디자인 추가 --1차

--- TeamProject/src/Source/Scar.java
+++ TeamProject/src/Source/Scar.java
@@ -1,6 +1,9 @@
 package Source;
 import java.awt.*;
 import javax.swing.*;
+
+import Source.Log.MyPanel;
+
 import java.awt.event.*;
 public class Scar extends JFrame{
 	private ImageIcon icon = new ImageIcon("images/sic.png");
@@ -10,20 +13,27 @@ public class Scar extends JFrame{
 	private JButton btn[] =new JButton[4];
 	private String name[] = {"양식","중식","일식","한식"};
 	private JLabel label[] = new JLabel[4];
+	private JPanel panel;
 	public Scar() {
 		setTitle("키오스크");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setLayout(null);
+		panel = new MyPanel("images/first main.png");
+		panel.setBackground(Color.WHITE);
+		panel.setLayout(null);
+		panel.setBounds(0, -30, 1500, 1000);
+		
 		for(int i=0;i<name.length;i++) {
 			btn[i] = new JButton();
-			btn[i].setBounds(370+(i*200),350,100,100);
+			btn[i].setBounds(370+(i*200),550,100,100);
 			btn[i].setIcon(change);
 			btn[i].setVisible(true);
+			btn[i].setBackground(Color.WHITE);
 			label[i] = new JLabel();
 			label[i].setText(name[i]);
-			label[i].setBounds(395+(i*200), 450, 70, 50);
+			label[i].setBounds(395+(i*200), 680, 70, 50);
 			label[i].setFont(new Font("배달의민족 한나", 1, 25));
-			add(btn[i]);add(label[i]);
+			panel.add(btn[i]);panel.add(label[i]);
 			btn[i].addActionListener(new ActionListener() {
 	            @Override
 	            public void actionPerformed(ActionEvent e) {
@@ -37,6 +47,7 @@ public class Scar extends JFrame{
 	            }
 	         });
 	      }
+		add(panel);
 	      setSize(1500,1000);
 	      setVisible(true);
 	      setLocationRelativeTo(null);
@@ -50,4 +61,23 @@ public class Scar extends JFrame{
 	       }
 	       return num;
 	    }
+	   class MyPanel extends JPanel {//이미지 변환을 위한 패널
+			Image image;
+
+			MyPanel(String img) {
+				image = Toolkit.getDefaultToolkit().createImage(img);
+			}
+
+			public void paintComponent(Graphics g) {
+				super.paintComponent(g);
+				if (image != null) {
+					g.drawImage(image, 0, 0, this);
+				}
+			}
+
+			public void update(Graphics g) {
+				paintComponent(g);
+			}
+
+		}// 마이패널 종료
 	}
